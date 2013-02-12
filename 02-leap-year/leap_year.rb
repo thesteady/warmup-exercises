7cb559c8d9523e2c381c9bf44e869209b469a6f2
Laura Steadman <lauramsteadman@gmail.com>
2013-02-12 09:25:32 -0700

    post-review

--- 02-leap-year/leap_year.rb
+++ 02-leap-year/leap_year.rb
@@ -3,30 +3,18 @@ class Year
   def initialize(input_year)
    @year = input_year
    puts @year.class
-
    puts "Your year is #{@year}."
   end
 
   def leap?
     puts "testing..."
 
-    if @year % 4 == 0
-      puts "Divisible by 4"
-      if @year % 100 == even?
-        #need to check if even
-        puts "Divisible by 100."
-        if @year % 400 == 00
-          puts "Divisible by 400."
-        else "Not a leap year 400."
-        end
-      else
-        puts "Not leap year."
-      end
-    else 
-      puts "Not a leap year."
-    end
+    divisible_by_4 = (@year % 4 == 0)
+    divisible_by_100 = (@year % 100 == 0)
+    divisible_by_400 = (@year % 400 == 0)
+
+    divisible_by_4 || (!divisible_by_100 || divisible_by_400)
   end
-end
 
 puts "Starting Leap Year Finder."
 Year.new(1996).leap?
