ca354cbc7cd60b539886e0dc22f4f8d809e358ed
Laura Steadman <lauramsteadman@gmail.com>
2013-02-14 09:16:05 -0700

    refactored bob.rb into a class

--- 04-bob/bob.rb
+++ 04-bob/bob.rb
@@ -1,17 +1,31 @@
-loop do
-printf "Say something to Bob:"
-input = gets.chomp
 
-  if input == "Bob"
-    puts "Fine. Be that way!"
-  elsif input == input.upcase
-    puts "Woah, chill out!"
-  elsif input.include? "?"
-    puts "Sure."
-  else
-    puts "Whatever."
+
+class Bob
+  
+  def initialize
+    loop do
+      printf "Say something to Bob:"
+      @input = gets.chomp
+      reply(@input)
+    end
   end
 
+  def reply(input)
+    if input == "Bob"
+      puts "Fine. Be that way!"
+    elsif input == input.upcase
+      puts "Woah, chill out!"
+    elsif input.include? "?"
+      puts "Sure."
+    else
+      puts "Whatever."
+    end
+  end
 
+  def l33t_sP34k(input)
+    puts @sep
+  end
 
 end
+
+Bob.new
